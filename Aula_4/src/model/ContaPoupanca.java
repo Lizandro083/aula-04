0228585e6db2105d8f8710458be32b6f289c4490
LAB2-PC 11 <LAB2-PC 11@DESKTOP-RKQDS19>
2024-02-26 21:56:04 -0300

    primeiro commit

---
+++ Aula_4/src/model/ContaPoupanca.java
@@ -0,0 +1,10 @@
+package model;
+
+public class ContaPoupanca extends conta{
+	private double rendimento;
+	ContaPoupanca (String email) {
+		super(email);
+		this.rendimento = rendimento; 
+	}
+ 
+}
