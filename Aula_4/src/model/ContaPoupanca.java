d8d2d580d8b675c2dd55114d43c9306f0a982278
LAB2-PC 11 <LAB2-PC 11@DESKTOP-RKQDS19>
2024-02-26 21:56:28 -0300

    primeiro commit

--- Aula_4/src/model/ContaPoupanca.java
+++ Aula_4/src/model/ContaPoupanca.java
@@ -1,10 +1,37 @@
 package model;
 
-public class ContaPoupanca extends conta{
+public class ContaPoupanca extends Conta{
 	private double rendimento;
 	ContaPoupanca (String email) {
 		super(email);
 		this.rendimento = rendimento; 
 	}
- 
+
+
+public String Saque (double valorSaque) { 
+	 if(valorSaque <= saldo) {
+	
+	  saldo =- valorSaque;
+	  return  (
+			  "Valor Sacado: " + valorSaque
+			  +"\nValor Saldo Após o Saque: "
+			  + saldo
+		);
+	 }  else {
+		  return "Saldo insuficiente";
+	 }
+	 
+}	 
+
+public String Deposito (double ValorDeposito) {
+	  saldo+= (ValorDeposito + (ValorDeposito *rendimento));
+	  return ("Valor Sacado: " + ValorDeposito
+			+"\nValor Saldo Após o Saque: "
+			+ saldo);
+}
+
+public double getSaldo() {
+	  return saldo;
+}
+
 }
